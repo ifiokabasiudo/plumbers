8d2eebc65a780b6f276e03244367caffcea13ece
ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2024-02-26 09:49:18 +0100

    Test 1

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -16,9 +16,13 @@ import Blog from "./src/components/Blog";
 import Discount from "./src/components/Discount";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import {Helmet} from "react-helmet";
 
 export default function Home() {
   const router = useRouter();
+
+//   ReactDOMServer.renderToString(<Handler />);
+// const helmet = Helmet.renderStatic();
 
   useEffect(() => {
     router.push("#");
@@ -26,9 +30,15 @@ export default function Home() {
 
   return (
     <>
-    <iframe src="https://embed.cody.bot/9b6c40f5-2a7b-4c3b-b68c-1b354ac81a23" style={{ border: '0px' }} name="codyai" scrolling="no" frameBorder="1" marginHeight={0} marginWidth={0} height="800px" width="100%" allowFullScreen></iframe>
+    {/* <iframe src="https://embed.cody.bot/9b6c40f5-2a7b-4c3b-b68c-1b354ac81a23" style={{ border: '0px' }} name="codyai" scrolling="no" frameBorder="1" marginHeight={0} marginWidth={0} height="800px" width="100%" allowFullScreen></iframe> */}
       <Discount />
       <main className="flex relative min-h-screen flex-col items-center font-montserrat tracking-wide text-[.9rem]">
+      <Helmet>
+        <script>
+          {`window.codySettings = { widget_id: '9b6c40f5-2a7b-4c3b-b68c-1b354ac81a23' };
+          !function(){var t=window,e=document,a=function(){var t=e.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://trinketsofcody.com/cody-widget.js";var a=e.getElementsByTagName("script")[0];a.parentNode.insertBefore(t,a)};"complete"===document.readyState?a():t.attachEvent?t.attachEvent("onload",a):t.addEventListener("load",a,!1)}();`}
+        </script>
+      </Helmet> 
       <Navbar />
       <SmMenu />
       <Hero />
